7041e81fc07fcd2ee3276d5eb3dd066bc99ba7a4
MuhammadShafiqAzuka <45938624+MuhammadShafiqAzuka@users.noreply.github.com>
2020-10-10 16:11:18 +0800

    create fucntion to return from viewmodel

--- app/src/main/java/com/azuka/mvvmapps/ui/job/viewmodel/JobViewModelFactory.kt
+++ app/src/main/java/com/azuka/mvvmapps/ui/job/viewmodel/JobViewModelFactory.kt
@@ -7,6 +7,9 @@ import com.azuka.mvvmapps.repositories.JobRepository
 class JobViewModelFactory(private val jobRepository: JobRepository) :
     ViewModelProvider.NewInstanceFactory() {
 
+    //THIS CLASS FOR RETURN THE LOGIC FUNCTION FROM VIEW MODEL
+    //ACT AS OBJECT TO RETURN
+
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         return JobViewModel(jobRepository) as T
