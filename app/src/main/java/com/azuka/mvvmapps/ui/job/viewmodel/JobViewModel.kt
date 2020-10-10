7550c95644a50e01a313f6a1af28e0cfb05eee7e
MuhammadShafiqAzuka <45938624+MuhammadShafiqAzuka@users.noreply.github.com>
2020-10-10 16:09:01 +0800

    create viewmodel handle from job repo

---
+++ app/src/main/java/com/azuka/mvvmapps/ui/job/viewmodel/JobViewModel.kt
@@ -0,0 +1,16 @@
+package com.azuka.mvvmapps.ui.job.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.azuka.mvvmapps.data.Job
+import com.azuka.mvvmapps.repositories.JobRepository
+
+class JobViewModel(private val jobRepository: JobRepository) : ViewModel() {
+
+    //THIS JOB VIEW MODEL CLASS ONLY HANDLE FROM REPOSITORIES
+
+    //get the job from job repo
+    fun getJob() = jobRepository.getJob()
+
+    //add the job data from job repo
+    fun addJob(job: Job) = jobRepository.addJob(job)
+}
