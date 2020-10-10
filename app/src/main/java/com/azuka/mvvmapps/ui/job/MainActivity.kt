2f07e8d1c68842dfd541bdb96bc38510f006c7b3
MuhammadShafiqAzuka <45938624+MuhammadShafiqAzuka@users.noreply.github.com>
2020-10-10 16:34:19 +0800

    init factory and viewmodel and pass to UI

--- app/src/main/java/com/azuka/mvvmapps/ui/job/MainActivity.kt
+++ app/src/main/java/com/azuka/mvvmapps/ui/job/MainActivity.kt
@@ -2,12 +2,42 @@ package com.azuka.mvvmapps.ui.job
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.azuka.mvvmapps.R
+import com.azuka.mvvmapps.data.Job
+import com.azuka.mvvmapps.ui.job.viewmodel.JobViewModel
+import com.azuka.mvvmapps.utilities.InjectionUtils
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        InitializeUI()
     }
+
+    private fun InitializeUI() {
+        //pass all injectors to here
+        val factory = InjectionUtils.provideJobViewModelFactory()
+
+        //use view model provider to pass from viewmodel class
+        val viewmodel = ViewModelProvider(this, factory)
+            .get(JobViewModel::class.java)
+
+        viewmodel.getJob().observe(this, { it ->
+            val stringBuilder = StringBuilder()
+            it.forEach {
+                stringBuilder.append("$it\n\n")
+            }
+            tvResult.text = stringBuilder.toString()
+        })
+        btnSend.setOnClickListener {
+            val job = Job(etName.text.toString(), etJob.text.toString())
+            viewmodel.addJob(job)
+            etName.setText("")
+            etJob.setText("")
+        }
+    }
+
 }
