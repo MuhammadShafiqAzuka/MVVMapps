cf75b4aea2ce93b88f33d602824201b9be7aa935
MuhammadShafiqAzuka <45938624+MuhammadShafiqAzuka@users.noreply.github.com>
2020-10-10 14:04:43 +0800

    Initial Commit

--- app/src/main/java/com/azuka/mvvmapps/ui/job/MainActivity.kt
+++ app/src/main/java/com/azuka/mvvmapps/ui/job/MainActivity.kt
@@ -1,12 +1,13 @@
-package com.azuka.mvvmapps
+package com.azuka.mvvmapps.ui.job
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.azuka.mvvmapps.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
     }
 }
